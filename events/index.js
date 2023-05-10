a99dce124f9e565e98e12d2540b91c77af72f888
Widyaapr <widyapp45@gmail.com>
2023-05-10 08:55:01 +0700

    Menambahkan events

---
+++ events/index.js
@@ -0,0 +1,13 @@
+const { EventEmitter } = require('events');
+
+const birthdayEventListener = (name) => {
+  console.log(`Happy birthday ${name}!`);
+};
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on('birthday', birthdayEventListener);
+
+myEmitter.emit('birthday', 'Widya');
+
+// terminal: node ./events/index.js
